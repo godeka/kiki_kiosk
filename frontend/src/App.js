158204ea3b8a1da77dadf347b7510bceb49cd974
godeka <godeka@naver.com>
2024-05-12 14:51:06 +0900

    [feat] 시작화면 생성 #1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
-import "./App.css";
+import { Container, Typography } from "@mui/material";
 
 function App() {
-  return <div className="App">Hello, I'm Kiki.</div>;
+  return (
+    <Container
+      sx={{
+        textAlign: "center",
+        padding: "50px",
+        background: `linear-gradient(0deg, #FFFFFF, #FF1E97)`, // 그라데이션 배경
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+      }}
+    >
+      <Typography variant="h3" style={{ color: "#FFFFFF", fontSize: "42px" }}>
+        어서오세요, <br />
+        키키 버거입니다.
+      </Typography>
+      <Typography variant="h5" style={{ color: "#000000", fontSize: "28px" }}>
+        주문을 시작하려면 <br />
+        화면을 터치해주세요.
+      </Typography>
+    </Container>
+  );
 }
 
 export default App;
