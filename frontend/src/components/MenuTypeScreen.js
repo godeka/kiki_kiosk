71a837bc8b78668f3fdf7ad29c5df6dfbad1d7c4
godeka <godeka@naver.com>
2024-05-13 12:13:01 +0900

    [feat] 메뉴 타입 선택 화면 추가 #5

---
+++ frontend/src/components/MenuTypeScreen.js
@@ -0,0 +1,41 @@
+import { Box, Grid } from "@mui/material";
+import { KebabDining, LunchDining, WineBar } from "@mui/icons-material";
+
+import GoBackButton from "./button/GoBackButton";
+import Question from "./Question";
+import BoxButton from "./button/BoxButton";
+
+export default function MenuType() {
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <GoBackButton />
+      <Question>
+        주문하실 메뉴의 <span style={{ color: "black" }}>종류</span>는 <br />
+        무엇인가요?
+      </Question>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <BoxButton text="햄버거">
+            <LunchDining style={{ fontSize: "70px" }} />
+          </BoxButton>
+        </Grid>
+        <Grid item xs={6}>
+          <BoxButton text="음료">
+            <WineBar style={{ fontSize: "70px" }} />
+          </BoxButton>
+        </Grid>
+        <Grid item xs={6}>
+          <BoxButton text="곁들여먹는 음식">
+            <KebabDining style={{ fontSize: "70px" }} />
+          </BoxButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
