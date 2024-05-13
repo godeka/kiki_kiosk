af6946f4b750d6737cd105890c330d871abf8e65
godeka <godeka@naver.com>
2024-05-13 12:27:08 +0900

    [feat] 결제방식 선택 화면 추가 #5

---
+++ frontend/src/components/PaymentMethodScreen.js
@@ -0,0 +1,37 @@
+import { Box, Grid } from "@mui/material";
+import { Money, Payment } from "@mui/icons-material";
+
+import GoBackButton from "./button/GoBackButton";
+import BoxButton from "./button/BoxButton";
+import Question from "./Question";
+
+export default function InOutScreen() {
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <GoBackButton />
+      <Question>
+        결제방식 선택
+        <br />
+        <br />
+      </Question>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <BoxButton text="현금">
+            <Money style={{ fontSize: "60px" }} />
+          </BoxButton>
+        </Grid>
+        <Grid item xs={6}>
+          <BoxButton text="카드">
+            <Payment style={{ fontSize: "60px" }} />
+          </BoxButton>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
