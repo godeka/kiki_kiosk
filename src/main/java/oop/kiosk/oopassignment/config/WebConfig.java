bfcf955bb741c742e96e09e73d568e759495850b
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-18 17:34:24 +0900

    Chore: CORS 설정

---
+++ src/main/java/oop/kiosk/oopassignment/config/WebConfig.java
@@ -0,0 +1,20 @@
+package oop.kiosk.oopassignment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000")
+                .allowedHeaders("OPTIONS", "GET", "POST", "PUT", "DELETE");
+//                .allowedHeaders("Authorization", "Content-Type")
+//                .allowCredentials(true);
+    }
+}
