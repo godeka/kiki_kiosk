85660fef4fa5f3cbbb3990b3ad828d5ce46fa9dc
imjanghyeok <qkdrhkgn23@naver.com>
2024-05-21 13:02:18 +0900

    Feature: Register 관련 API 작성

--- src/main/java/oop/kiosk/oopassignment/register/RegisterRepository.java
+++ src/main/java/oop/kiosk/oopassignment/register/RegisterRepository.java
@@ -1,4 +1,10 @@
 package oop.kiosk.oopassignment.register;
 
-public interface RegisterRepository {
+import oop.kiosk.oopassignment.register.domain.Register;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RegisterRepository extends JpaRepository<Register, Long> {
+    Optional<Register> findRegisterByPassword(String password);
 }
---
+++ src/main/java/oop/kiosk/oopassignment/register/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package oop.kiosk.oopassignment.register.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String password;
+}
