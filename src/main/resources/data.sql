b80aec6475ce0f60ae2d45cb9eac96a4b4d75226
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-05-22 14:11:58 +0900

    [fix] 서버 킬때마다 menu data 자동 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('키키버거', 4500, 'HAMBURGER', '', 'burger/basicBurger.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('대왕버거', 6500, 'HAMBURGER', '', 'burger/bigBurger.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('치즈베이컨버거', 5500, 'HAMBURGER', '', 'burger/cheeseBaconBurger.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('채식버거', 5000, 'HAMBURGER', '', 'burger/beganBurger.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('치즈왕창버거', 5000, 'HAMBURGER', '', 'burger/doubleCheeseBurger.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('코카콜라', 2000, 'DRINK', '', 'beverage/cocaCola.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('사이다', 2000, 'DRINK', '', 'beverage/sprite.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('환타', 2200, 'DRINK', '', 'beverage/fanta.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('닥터페퍼', 2200, 'DRINK', '', 'beverage/doctorPepper.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('레몬에이드', 3500, 'DRINK', '', 'beverage/lemonAde.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('아이스티', 3000, 'DRINK', '', 'beverage/iceTea.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('커피', 2500, 'DRINK', '', 'beverage/coffee.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('감자튀김', 2000, 'SIDE', '', 'side/fries.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('치즈볼', 2500, 'SIDE', '', 'side/cheeseBall.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('해시브라운', 2500, 'SIDE', '', 'side/hashBrown.png');
+INSERT INTO menu (name, price, type, info, imageUrl) VALUES ('샐러드', 5500, 'SIDE', '', 'side/salad.png');
