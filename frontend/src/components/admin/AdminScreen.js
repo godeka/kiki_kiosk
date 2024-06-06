1e752b279a1562382edf8ee87c40be201facd030
godeka <godeka@naver.com>
2024-06-06 15:33:18 +0900

    [feat] 관리자 권한 페이지 추가 #13

---
+++ frontend/src/components/admin/AdminScreen.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Tabs,
+  Tab,
+} from "@mui/material";
+
+import Login from "./Login";
+
+export default function AdminScreen({ setContents }) {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setTabIndex(newValue);
+  };
+
+  return (
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            키키 키오스크 관리자 페이지
+          </Typography>
+          <Button color="inherit">비밀번호 변경</Button>
+          <Button
+            color="inherit"
+            onClick={() => {
+              setContents(<Login />);
+            }}
+          >
+            로그아웃
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <Tabs
+          value={tabIndex}
+          onChange={handleTabChange}
+          centered
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="메뉴 관리" />
+          <Tab label="정산" />
+        </Tabs>
+      </Box>
+      <Box sx={{ p: 3 }}>
+        {tabIndex === 0 && <Typography>메뉴 관리 내용</Typography>}
+        {tabIndex === 1 && <Typography>정산 내용</Typography>}
+      </Box>
+    </Container>
+  );
+}
