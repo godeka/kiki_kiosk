14b79550c46a7ad48d4a7649121434f1ebda998e
imjanghyeok <qkdrhkgn23@naver.com>
2024-06-09 17:31:49 +0900

    fix: body에 response 미포함 문제

--- src/main/java/oop/kiosk/oopassignment/register/RegisterController.java
+++ src/main/java/oop/kiosk/oopassignment/register/RegisterController.java
@@ -4,9 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import oop.kiosk.oopassignment.register.dto.LoginRequest;
 import oop.kiosk.oopassignment.register.dto.PasswordChangeRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
@@ -18,21 +22,30 @@ public class RegisterController {
             @RequestBody LoginRequest loginRequest,
             final HttpServletRequest request
     ) {
-        return new ResponseEntity<>(registerService.login(request, loginRequest.getPassword()), HttpStatus.OK);
+        String response = registerService.login(request, loginRequest.getPassword());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
 
     @PostMapping("/api/logout")
     public ResponseEntity<String> logout(
             final HttpServletRequest request
     ) {
-        return new ResponseEntity<>(registerService.logout(request), HttpStatus.OK);
+        String response = registerService.logout(request);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
+
     @PutMapping("/api/password-change")
     public ResponseEntity<String> putPassword(
             @RequestBody PasswordChangeRequest passwordChangeRequest,
             final HttpServletRequest request
     ) {
-        return new ResponseEntity<>(registerService.changePassword(request, passwordChangeRequest), HttpStatus.OK);
+        String response = registerService.changePassword(request, passwordChangeRequest);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
-
 }
